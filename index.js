f29e00e78aec7c7704a2f5bc47b281cb18627b71
gean-maximo <geanmaximos@gmail.com>
2024-09-11 21:10:45 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = {
     value: 'Tomar 3L de agua por dia',
@@ -19,24 +19,70 @@ const cadastrarMeta = async () => {
         { value: meta, checked: false }
     )
 
-}
+} 
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false, 
+    }) 
+
+    if(respostas.length == 0) {
+        console.log("nenhuma meta selecionada!")
+        return 
+    }
+
+    metas.forEach((m) => {
+        m.checked = false 
+    })
+
+
+
+    respostas.forEach((respostas) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta 
+        })
+
+        meta.checked = true 
+    } ) 
+
+    console.log('Meta(s) marcadas como concluida(s)')
+} 
 
 const start = async () => {
 
+    
+
     while(true){
 
-        const opcao = await select({
+        const opcao = await select({message: "menu >",
+            choices: [
+                {
+                    name: "cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "listar metas",
+                    value: "listar",
+                },
+                {
+                    name:"Sair",
+                    value:"sair"
+                }
+            ]
         })
+        
 
 
-
+        
         switch(opcao) {
             case "cadastrar":
                 await cadastrarMeta()
                 console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")
+                await listarMetas()
                 break
             case "sair":
                 console.log("ate a proxima")
